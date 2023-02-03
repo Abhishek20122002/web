096a2684ba59ae7ea4cdda1a572bbbff033ef02f
Abhishek20122002 <98834249+Abhishek20122002@users.noreply.github.com>
2023-02-03 23:36:36 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+console.log('Abhishek Mishra');
+console.log(23);
+console.log('Abhishek');
+if(23!=='23')console.log('true');
+const nameOfstudents=['Abhi','Shivam','ganesh','komal'];
+console.log(nameOfstudents);
